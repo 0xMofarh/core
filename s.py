9bf42e917dcc9fbabca2d7a4afb73e9ba72d0258
Andu <114863878+0xMofarh@users.noreply.github.com>
2025-10-18 13:00:12 +0300

    Delete s.py

--- s.py
+++
@@ -1,5 +0,0 @@
-import secrets
-alphabet = 'abcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*(-_=+)'
-key_gen = (secrets.choice(alphabet) for _ in range(90))
-key = ''.join(key_gen)  # هنا نحول الـ generator لسلسلة نصية
-print(key)
