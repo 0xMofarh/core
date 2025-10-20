d755909516df42210708cff8db91b1c39dd7dc08
Andu <114863878+0xMofarh@users.noreply.github.com>
2025-10-20 13:59:15 +0300

    Update urls.py

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -7,7 +7,7 @@ app_name = 'dashboard'
 urlpatterns = [
     path('', views.dashboard_home, name='dashboard'),
     path('influencers/', lambda request: redirect('influencers:influencers_search')),
-    path('/campaigns/', lambda request: redirect('campaigns:campaigns_list')),
+    path('campaigns/', lambda request: redirect('campaigns:campaigns_list')),
     path('reports/', views.reports_view, name='reports'),
     path('profile/', views.profile_view, name='profile'),
     path('settings/', views.settings_view, name='settings'),
